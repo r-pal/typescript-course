c808bf17988d68db3f6b109b4a73a7697d31da2c
r-pal <90906037+r-pal@users.noreply.github.com>
2023-08-08 17:00:18 +0100

    config

--- app.ts
+++ app.ts
@@ -1,16 +1,22 @@
-let userInput: unknown;
+// let userInput: unknown;
 
-let userName: string;
+// let userName: string;
 
-userInput = 5;
-userInput = "hello";
-if (typeof userInput === "string"){
-    userName = userInput;
-}
+// userInput = 5;
+// userInput = "hello";
+// if (typeof userInput === "string"){
+//     userName = userInput;
+// }
 
-function generateError(message: string, code: number): never {
-    // throw {message: message, errorCode: code};
-    while (true) {}
-}
+// function generateError(message: string, code: number): never {
+//     // throw {message: message, errorCode: code};
+//     while (true) {}
+// }
 
-generateError("An error occurred!", 500);
+// generateError("An error occurred!", 500);
+
+const button = document.querySelector("button");
+
+button?.addEventListener('click', () => {
+    console.log('Clicked!');
+});
